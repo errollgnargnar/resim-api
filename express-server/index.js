16ed832e59aa6de01ed3fbd25088b74526cff872
Eric Roldan <errollgnargnar@gmail.com>
2022-10-10 01:46:43 -0500

    full stack draft push a

---
+++ express-server/index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const { exec } = require("child_process");
+
+const app = express()
+app.use(express.json());
+const port = 3000
+
+let accounts = [];
+let packages = [];
+
+app.get('/reset', (req,res) => {
+  accounts = [];
+  packages = [];
+  exec("bash ../shscripts/index.sh reset", (err, stdout,stderr) => {
+    if (err) {
+      console.log(`error: ${err.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    let newAccnt = stdout.split(' ');
+    accounts.push(newAccnt);
+    accounts[accounts.length-1][2] = accounts[accounts.length-1][2].replace('\n','');
+    res.send(accounts);
+  })
+});
+
+/*
+POST publish example:
+curl -X POST localhost:3000/publish -H "Content-Type: application/json" -d '{"dir": "~/radix_dev/the_a_team/dao/scrypto/", "packageName": "ace treasury"}'
+*/
+app.post('/publish', (req, res) => {
+  const dir = req.body.dir;
+  const packageName = req.body.packageName;
+  const fullCmd = `bash ../shscripts/index.sh publish ${dir} "${packageName}"`;
+  exec(fullCmd, (err, stdout, stderr) => {
+    packages.push(stdout.split(','));
+    packages[packages.length-1][1] = packages[packages.length-1][1].replace('\n','');
+    res.send(packages);
+  });
+  // res.sendStatus(200);
+});
+
+app.get('/getaccounts', (req, res) => {
+  res.send(accounts);
+})
+
+app.get('/newaccount', (req,res) => {
+  exec("bash ../shscripts/index.sh resnpub", (err, stdout,stderr) => {
+    if (err) {
+      console.log(`error: ${err.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    let newAccnt = stdout.split(' ');
+    accounts.push(newAccnt);
+    accounts[accounts.length-1][2] = accounts[accounts.length-1][2].replace('\n','');
+    res.send(accounts);
+  })
+});
+
+app.get('/newtreasury', (req, res) => {
+  exec(`bash ../shscripts/index.sh new_treasury ${packages[0][0]}`, (err, stdout, stderr) => {
+    res.send(stdout);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ shscripts/index.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+newaccnt() {
+  batch=$(echo $(resim new-account | grep "account\|key" | awk -F": " '{print $2}' | awk 'NF'))
+  echo $batch
+}
+
+publish() {
+  batch=$(resim publish "$1");
+  package_id=$(echo "$batch: " | awk -F ": " '{print $2}')
+  package_name="$2"
+  package=($package_id,"$package_name")
+  echo $package
+}
+
+reset() {
+  resim reset > /dev/null
+  batch=$(echo $(resim new-account | grep "account\|key" | awk -F": " '{print $2,$3}' | awk 'NF'))
+  echo $batch
+}
+
+new_treasury() {
+  resim call-function "$1" Treasury new
+}
+
+"$@" # this allows for functions to be called from the command line when running the script. 
